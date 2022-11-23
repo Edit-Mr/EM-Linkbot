aee8b07734d6ac8a451ac47941639cf0c4281e96
Edit-Mr <elvismao.070512@gmail.com>
2022-11-23 11:15:27 +0800

    v1.0

---
+++ index.js
@@ -0,0 +1,75 @@
+function doPost(e) {
+    var channelToken = "";
+    var value = JSON.parse(e.postData.contents);
+    try {
+      var SpreadSheet = SpreadsheetApp.openById("1wAL-Nn6zq4ERP1zZQRQILWQcdwBsNHQh5vwz9H3Yov8");
+      var Sheet = SpreadSheet.getSheets()[0];
+      var events = value.events;
+      if (events != null) {
+        for (var i in events) {
+          var event = events[i];
+          var type = event.type;
+          var replyToken = event.replyToken; //
+          var userId = event.source.userId; // 取得個人userId
+          var groupId = event.source.groupId; // 取得群組Id
+          var LastRow = Sheet.getLastRow();
+          Sheet.getRange(LastRow + 1, 1).setValue(new Date());
+          Sheet.getRange(LastRow + 1, 2).setValue(userId);
+          Sheet.getRange(LastRow + 1, 3).setValue(event.message.text);
+          Sheet.getRange(LastRow + 1, 4).setValue(replyToken);
+          Sheet.getRange(LastRow + 1, 5).setValue(groupId);
+          switch (type) {
+            case "message":
+              var a = event.message.text;
+              if ((a.indexOf("[[") > -1 && a.indexOf("]]") > -1) || (a.indexOf("{{") > -1 && a.indexOf("}}") > -1)) {
+                var response = [];
+                var list = a.split("[[");
+                for (var i = 1; i < list.length; i++) {
+                  var text = list[i];
+                  response.push("https://zh.m.wikipedia.org/wiki/" + text.split("]]")[0]);
+                }
+                list = a.split("{{");
+                for (i = 1; i < list.length; i++) {
+                  text = list[i];
+                  response.push("https://zh.m.wikipedia.org/wiki/Template:" + text.split("}}")[0]);
+                }
+                meg = [{
+                  type: "text",
+                  text: response.join("\n")
+                }];
+                Sheet.getRange(LastRow + 1, 6).setValue(meg);
+                replyMsg(replyToken, meg, channelToken);
+              } else Sheet.getRange(LastRow + 1, 6).setValue("No reply");
+              break;
+            default:
+              Sheet.getRange(LastRow + 1, 6).setValue("Not Text");
+              break;
+          }
+  
+        }
+      }
+    } catch (ex) {
+      Sheet.getRange(LastRow + 1, 7).setValue(ex);
+    }
+  }
+  // 回覆訊息
+  function replyMsg(replyToken, userMsg, channelToken) {
+    try {
+      var url = "https://api.line.me/v2/bot/message/reply";
+      var opt = {
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+          Authorization: "Bearer " + channelToken,
+        },
+        method: "post",
+        payload: JSON.stringify({
+          replyToken: replyToken,
+          muteHttpExceptions: true,
+          messages: userMsg,
+        }),
+      };
+      UrlFetchApp.fetch(url, opt);
+    } catch (ex) {
+      Sheet.getRange(LastRow + 1, 7).setValue(ex);
+    }
+  }
